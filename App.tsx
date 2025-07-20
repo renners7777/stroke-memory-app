af89adccc02fe4f8e775abda1e12e4367974e789
chrisrenshaw79 <chrisrenshaw79@gmail.com>
2025-07-20 22:40:00 +0000

    Updated .env environment variables

--- App.tsx
+++ App.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import firebase from '@react-native-firebase/app';
-
+// Import environment variables from @env
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from '@env';
 
 
 // Your Firebase project configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -22,7 +31,7 @@ if (!firebase.apps.length) {
   firebase.app(); // if already initialized, use that one
 }
 
-
+// ... rest of your App component
 const App = () => {
   return (
     <View style={styles.container}>
