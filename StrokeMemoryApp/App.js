85644a87e08b6cb9fee4ed102d152d00f6239b06
chrisrenshaw79 <chrisrenshaw79@gmail.com>
2025-07-20 21:37:20 +0000

    Changes made to app structure

---
+++ StrokeMemoryApp/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to your React Native App!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
