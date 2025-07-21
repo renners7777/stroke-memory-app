8df4c2a666e4af114816bdee5106a19def9af2f6
chrisrenshaw79 <chrisrenshaw79@gmail.com>
2025-07-21 21:56:48 +0000

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,9 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('@expo/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// Add any custom configurations here
+
+module.exports = defaultConfig;
+
