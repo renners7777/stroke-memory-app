7f676e5bd201b3f18cba6147ed48c9ffc703ac25
chrisrenshaw79 <chrisrenshaw79@gmail.com>
2025-07-22 22:15:52 +0000

    Updated index.js to specifically render web app

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform } from 'react-native'; // Import Platform
 import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// For web, explicitly render the app
+if (Platform.OS === 'web') {
+  AppRegistry.runApplication(appName, {
+    rootTag: document.getElementById('root'),
+  });
+} else {
+  // For native platforms, just register the component
+  AppRegistry.registerComponent(appName, () => App);
+}
