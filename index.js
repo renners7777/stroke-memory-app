e182b3b7e5b691cd4743d987d47953b47be6b2a3
chrisrenshaw79 <chrisrenshaw79@gmail.com>
2025-07-22 22:21:52 +0000

    Updated index.js again

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
  * @format
  */
 
-import { AppRegistry, Platform } from 'react-native'; // Import Platform
+import { AppRegistry, Platform } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import appJson from './app.json'; // Import the entire app.json object
+
+// Access the app name from the imported object
+const appName = appJson.expo.name;
 
 // For web, explicitly render the app
 if (Platform.OS === 'web') {
